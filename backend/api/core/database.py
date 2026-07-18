01ccccac8665722800230c66542f4fb526b3dc3a
Zain <zain.9496@gmail.com>
2026-07-18 23:52:18 +0500

    changed db to support env vars

--- backend/api/core/database.py
+++ backend/api/core/database.py
@@ -1,13 +1,19 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import NullPool
 
-# SQLite (for simplicity). For PostgreSQL:
-# DATABASE_URL = "postgresql://user:password@localhost/dbname"
-DATABASE_URL = "sqlite:///./clipboard.db"
+DATABASE_URL = os.environ.get("DATABASE_URL", "sqlite:///./clipboard.db")
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+is_sqlite = DATABASE_URL.startswith("sqlite")
 
 engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}  # only for SQLite
+    DATABASE_URL,
+    connect_args={"check_same_thread": False} if is_sqlite else {},
+    poolclass=None if is_sqlite else NullPool,
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
